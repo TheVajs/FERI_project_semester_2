815e01ecfb76f4257960979341ba501cc644fd35
Simon <simonvajsfb@gmail.com>
2019-06-11 18:40:57 +0200

    Event Planer 2.3
    - Fixes in login
    - Login in return result of image processing and events(if image similarity is bigger than .7)
    - The new events are added to shared preference

--- app/src/main/java/com/example/feriproject/LoginActivity.java
+++ app/src/main/java/com/example/feriproject/LoginActivity.java
@@ -2,12 +2,14 @@ package com.example.feriproject;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,28 +18,13 @@ import android.widget.Toast;
 
 import com.github.sundeepk.compactcalendarview.domain.Event;
 import com.google.gson.Gson;
-
-import org.greenrobot.eventbus.EventBus;
-
-import java.io.ByteArrayOutputStream;
-import java.io.Console;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.UnknownFormatFlagsException;
-
-import okhttp3.Call;
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button button_login, button_skip;
     EditText input_name, input_password, input_link;
+    Handler mHandler;
 
     // help
     // https://www.youtube.com/watch?v=cjNW2noShM0
@@ -94,6 +81,13 @@ public class LoginActivity extends AppCompatActivity {
                 skip();
             }
         });
+
+        mHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message message) {
+                Toast.makeText(getBaseContext(), message.obj + " :)", Toast.LENGTH_SHORT).show();
+            }
+        };
     }
 
     private void skip() {
@@ -117,7 +111,7 @@ public class LoginActivity extends AppCompatActivity {
             String response = MyHttp.doPostRequest(MyHttp._GETTOKEN, json);
 
             Log.d(MyApplication.TAG, "LOGIN : " + response);
-            if(response.length() < 40) throw new IllegalStateException();
+            if(response.length() < 40 || response.contains("Could not verify")) throw new IllegalStateException();
 
             MyHttp.TOKEN = response.substring(10, response.length()-2);
 
@@ -152,30 +146,64 @@ public class LoginActivity extends AppCompatActivity {
                 if (resultCode == RESULT_OK) {
 
                     /* SEND IMAGE TO SERVER */
-                    try
-                    {
-                        /* CONVERT IMAGE TO STRING */
-                        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-                        if(bitmap==null) {
-                            Log.d(MyApplication.TAG, "onActivityResult: bitmap null");
-                            return;
-                        }
-                        String encodedImage = MyHttp.bitmapToImage(bitmap);
+                    Runnable runable = new Runnable(){
+                        public void run(){
+                            Log.d(MyApplication.TAG, "THREAD RUNNING!");
+                            try
+                            {
+                                /* CONVERT IMAGE TO STRING */
+                                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                                if(bitmap==null) {
+                                    Log.d(MyApplication.TAG, "onActivityResult: bitmap null");
+                                    return;
+                                }
+                                Log.d(MyApplication.TAG, "onActivityResult: bitmap dimensions: " + bitmap.getHeight() + "x" + bitmap.getWidth());
+                                String encodedImage = MyHttp.bitmapToImage(bitmap);
 
-                        /* CHECK IMAGE */
-                        Gson gson = new Gson();
-                        String json = gson.toJson(new ImageMessage("newSimon.jpg", encodedImage));
-                        String request = MyHttp.doPostRequest(MyHttp._CHECKIMAGE, json, MyHttp.TOKEN);
-                        Toast.makeText(this, request + " :)", Toast.LENGTH_SHORT).show();
-                    }
-                    catch (Exception e)
-                    {
-                        Log.d(MyApplication.TAG, "sendImage(" + MyHttp._CHECKIMAGE + "): " + e.getMessage() + " | ");
-                        Toast.makeText(this, "Can't sent image", Toast.LENGTH_SHORT).show();
-                        e.printStackTrace();
-                    }
-                    break;
+                                /* CHECK IMAGE */
+                                Gson gson = new Gson();
+                                String json = gson.toJson(new ImageMessage("newSimon.jpg", encodedImage));
+                                String request = MyHttp.doPostRequest(MyHttp._CHECKIMAGE, json, MyHttp.TOKEN);
+
+                                String results = "", data = "";
+
+                                if(request.contains("_")) {
+                                    String split[] = request.split("_");
+                                    results = split[0] + "\n OK!";
+                                    data = split[1];
+                                    Log.d(MyApplication.TAG, "sendImage(" + MyHttp._CHECKIMAGE + ") contains(|): " + data);
+                                    Gson g = new Gson();
+                                    List<Event> eventList = g.fromJson(data, MyApplication.eventType);
+                                    for (int i = 0; i < eventList.size(); i++) {
+                                        long time = eventList.get(i).getTimeInMillis();
+                                        Object o = eventList.get(i).getData();
+                                        Event nEvent = new Event(Color.MAGENTA, time, o);
+                                        eventList.set(i, nEvent);
+                                    }
+                                    // save new DATA
+                                    MyData myData = ((MyApplication)getApplication()).getData();
+                                    for(Event event: eventList) myData.addEvent(event);
+                                    ((MyApplication)getApplication()).saveMain();
+
+                                    Log.d(MyApplication.TAG, "SERVER RESULT: " + data + "\n" + eventList.size());
+                                } else {
+                                    results = request +"\n NOPE!";
+                                }
+                                Message message = mHandler.obtainMessage(0,results);
+                                message.sendToTarget();
+                            }
+                            catch (Exception e)
+                            {
+                                mHandler.obtainMessage(0,"Can't send image").sendToTarget();
+                                Log.d(MyApplication.TAG, "sendImage(" + MyHttp._CHECKIMAGE + "): " + e.getMessage() + " | ");
+                                //Toast.makeText(getBaseContext(), "Can't sent image", Toast.LENGTH_SHORT).show();
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+                    new Thread(runable).start();
                 }
+                break;
         }
     }
 }
